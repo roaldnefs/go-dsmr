1cd0f9f2a099fc266c65afa4577d4b62999c0d7e
Roald Nefs <info@roaldnefs.com>
2019-07-22 14:48:38 +0200

    Add additional functions
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- telegram.go
+++ telegram.go
@@ -32,6 +32,155 @@ type Telegram struct {
 	DataObjects map[string]DataObject
 }
 
+// MeterReadingElectricityDeliveredToClientTariff1 returns the meter reading
+// electricity delivered to client (Tariff 1) in 0,001 kWh.
+func (t Telegram) MeterReadingElectricityDeliveredToClientTariff1() (string, bool) {
+	if do, ok := t.DataObjects["1-0:1.8.1"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// MeterReadingElectricityDeliveredToClientTariff2 returns the meter reading
+// electricity delivered to client (Tariff 2) in 0,001 kWh.
+func (t Telegram) MeterReadingElectricityDeliveredToClientTariff2() (string, bool) {
+	if do, ok := t.DataObjects["1-0:1.8.2"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// MeterReadingElectricityDeliveredByClientTariff1 returns the meter reading
+// electricity delivered by client (Tariff 1) in 0,001 kWh.
+func (t Telegram) MeterReadingElectricityDeliveredByClientTariff1() (string, bool) {
+	if do, ok := t.DataObjects["1-0:2.8.1"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// MeterReadingElectricityDeliveredByClientTariff2 returns the meter reading
+// electricity delivered by client (Tariff 2) in 0,001 kWh.
+func (t Telegram) MeterReadingElectricityDeliveredByClientTariff2() (string, bool) {
+	if do, ok := t.DataObjects["1-0:2.8.2"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// TariffIndicatorElectricity returns the tariff indicator that can be used to
+// switch tariff dependent loads e.g. boilers.
+func (t Telegram) TariffIndicatorElectricity() (string, bool) {
+	if do, ok := t.DataObjects["0-0:96.14.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// ActualElectricityPowerDelivered returns the actual electricity power
+// delivered (+P) in 1 Watt resolution.
+func (t Telegram) ActualElectricityPowerDelivered() (string, bool) {
+	if do, ok := t.DataObjects["1-0:1.7.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// ActualElectricityPowerReceived returns the actual electricity power
+// received (-P) in 1 Watt resolution.
+func (t Telegram) ActualElectricityPowerReceived() (string, bool) {
+	if do, ok := t.DataObjects["1-0:2.7.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfPowerFailuresInAnyPhase returns the number of power failures in any
+// phase.
+func (t Telegram) NumberOfPowerFailuresInAnyPhase() (string, bool) {
+	if do, ok := t.DataObjects["0-0:96.7.21"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfLongPowerFailuresInAnyPhase returns the number of power failures in any
+// phase.
+func (t Telegram) NumberOfLongPowerFailuresInAnyPhase() (string, bool) {
+	if do, ok := t.DataObjects["0-0:96.7.9"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// TODO update regex so it will match:
+//   None: 1-0:99.97.0()
+//   Two: 1-0:99.97.0(2)(0-0:96.7.19)(101208152415W)(0000000240*s)(101208151004W)(0000000301*s)
+// PowerFailureEventLog returns the power failure event log (long power
+// failures).
+func (t Telegram) PowerFailureEventLog() (string, bool) {
+	if do, ok := t.DataObjects["1-0:99.97.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSagsInPhaseL1 returns the number of voltage sags in phase L1.
+func (t Telegram) NumberOfVoltageSagsInPhaseL1() (string, bool) {
+	if do, ok := t.DataObjects["1-0:32.32.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSagsInPhaseL2 returns the number of voltage sags in phase L2.
+func (t Telegram) NumberOfVoltageSagsInPhaseL2() (string, bool) {
+	if do, ok := t.DataObjects["1-0:52.32.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSagsInPhaseL3 returns the number of voltage sags in phase L3.
+func (t Telegram) NumberOfVoltageSagsInPhaseL3() (string, bool) {
+	if do, ok := t.DataObjects["1-0:72.32.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSwellsInPhaseL1 returns the number of voltage swells in phase L1.
+func (t Telegram) NumberOfVoltageSwellsInPhaseL1() (string, bool) {
+	if do, ok := t.DataObjects["1-0:32.36.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSwellsInPhaseL2 returns the number of voltage swells in phase L2.
+func (t Telegram) NumberOfVoltageSwellsInPhaseL2() (string, bool) {
+	if do, ok := t.DataObjects["1-0:52.36.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// NumberOfVoltageSwellsInPhaseL3 returns the number of voltage swells in phase L3.
+func (t Telegram) NumberOfVoltageSwellsInPhaseL3() (string, bool) {
+	if do, ok := t.DataObjects["1-0:72.36.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
+// TextMessage returns text message of max 1024 characters.
+func (t Telegram) TextMessage() (string, bool) {
+	if do, ok := t.DataObjects["0-0:96.13.0"]; ok {
+		return do.Value, true
+	}
+	return "", false
+}
+
 // InstantaneousVoltageL1 returns the instantaneous voltage L1 in V resolution.
 func (t Telegram) InstantaneousVoltageL1() (string, bool) {
 	if do, ok := t.DataObjects["1-0:32.7.0"]; ok {
